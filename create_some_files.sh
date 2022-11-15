36127b397a4f52f307a3560c5bb1bc7dc6120586
thaihainguyen <58269491+thaihainguyen@users.noreply.github.com>
2022-11-14 20:54:16 -0600

    Update create_some_files.sh

--- create_some_files.sh
+++ create_some_files.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkdir some-files
-echo "file1" > some-files/file1
-echo "file2" > some-files/file2
-echo "file3" > some-files/file3
-echo "file4" > some-files/file4
+mkdir myfiles
+echo "file1" > myfiles/file1
+echo "file2" > myfiles/file2
+echo "file3" > myfiles/file3
+echo "file4" > myfiles/file4
 
-ls some-files/*
+ls myfiles/*
